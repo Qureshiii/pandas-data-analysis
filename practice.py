ddee79518e2d55f7c3fc65cdb61a7a6d90d748c4
Muhammad Muttahir Qureshi <154008678+Qureshiii@users.noreply.github.com>
2026-09-24 14:08:53 +0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,14 @@
+x = 5
+y = 10
+
+if x > 2:
+    if y > 5:
+        if x + y == 15:
+            print('Epstein')
+        else:
+            print('Goat Cristiano')
+    else:
+        print('Legend Messi')
+        
+else:
+    print('Donald Trump')
